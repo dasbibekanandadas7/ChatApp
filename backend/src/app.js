ad95dd330a233db7fdba8c27efcdcf1855297615
Bibekananda Das <72216905+dasbibekanandadas7@users.noreply.github.com>
2025-11-17 00:03:52 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import messageRouter from './routes/message.routes.js';
 export const app=express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chatapp-frontend-ic1n.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
